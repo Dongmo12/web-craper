7cf76fcd5ec804c699cafa55bf047f8282c1aa8c
Dongmo12 <dongmoarmand@gmail.com>
2022-08-11 11:39:23 +0100

    Pushing last code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
+
+my_url = "https://sponsor.hackernoon.com/billboard"
+
+response = requests.get(url=my_url)
+data = response.text
+
+soup = BeautifulSoup(data, "html.parser")
+my_title = soup.find(name="h1")
+my_paragraph = soup.find(name="p")
+my_content = soup.find(name="div")
+print(my_title.getText())
+print(my_paragraph.getText())
+print(my_content.getText())
